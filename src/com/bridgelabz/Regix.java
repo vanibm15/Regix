2854043d73e1d6e56ac1bd121262c0a76a834818
vanibm15 <vanibm15@gmail.com>
2022-05-12 19:04:07 +0530

    added:special char to password

--- src/com/bridgelabz/Regix.java
+++ src/com/bridgelabz/Regix.java
@@ -3,10 +3,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Regix {
 
-    static Pattern pattern=Pattern.compile("^([0-9]*[a-zA-Z]){3,}[0-9a-zA-Z]*$");
+    static Pattern pattern=Pattern.compile("^([0-9]*[a-zA-Z]){3,}(@)[0-9a-zA-Z]*$");
 
     public static void main(String[] args) {
-        String testString="Vanishree1";
+        String testString="Vani@shree1";
         Matcher matcher= pattern.matcher(testString);
         if (matcher.matches()){
             System.out.println("Matches");
