50efe459520821866fc75fdff03748be5cf08409
vanibm15 <vanibm15@gmail.com>
2022-05-12 13:32:41 +0530

    added:mobileformat

--- src/com/bridgelabz/Regix.java
+++ src/com/bridgelabz/Regix.java
@@ -3,13 +3,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Regix {
 
-    static Pattern pattern=Pattern.compile("abc.[a-z]{3,}@bl.co.[a-z]{2}");
+    static Pattern pattern=Pattern.compile("[0-9]{2} [0-9]{10}");
 
     public static void main(String[] args) {
-        String testString="abc.zzz@bl.co.in";
+        String testString="91 1234567895";
         Matcher matcher= pattern.matcher(testString);
         if (matcher.matches()){
             System.out.println("Matches");
+
         }
         else {
             System.out.println("not matching");
