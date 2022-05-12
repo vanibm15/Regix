cf43d3a0052bc7c205ab11d8b3a8beb1754d0b9c
vanibm15 <vanibm15@gmail.com>
2022-05-12 12:49:32 +0530

    added:cap first name

---
+++ src/com/bridgelabz/Regix.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Regix {
+
+    static Pattern pattern=Pattern.compile("^([A-Z]){3,}$");
+
+    public static void main(String[] args) {
+        String testString="VANI";
+        Matcher matcher= pattern.matcher(testString);
+        if (matcher.matches()){
+            System.out.println("Matches");
+        }
+        else {
+            System.out.println("not matching");
+        }
+    }
+}
+
