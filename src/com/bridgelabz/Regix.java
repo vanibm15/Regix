9be466adefb53fbb39928901ffbc76b2b8016677
vanibm15 <vanibm15@gmail.com>
2022-05-12 18:44:00 +0530

    added:passwordrules

--- src/com/bridgelabz/Regix.java
+++ src/com/bridgelabz/Regix.java
@@ -3,14 +3,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Regix {
 
-    static Pattern pattern=Pattern.compile("[0-9]{2} [0-9]{10}");
+    static Pattern pattern=Pattern.compile("^([0-9]*[a-zA-Z]){3,}[0-9a-zA-Z]*$");
 
     public static void main(String[] args) {
-        String testString="91 1234567895";
+        String testString="vanishree";
         Matcher matcher= pattern.matcher(testString);
         if (matcher.matches()){
             System.out.println("Matches");
-
         }
         else {
             System.out.println("not matching");
